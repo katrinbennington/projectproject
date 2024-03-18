f3f662857aab694c59fffc51ad91ea3339c90b54
Katrin <katrin_bennington@mail.ru>
2024-03-18 22:13:45 +0500

    message

--- src/category.py
+++ src/category.py
@@ -1,14 +1,17 @@
 class Category:
     cnt_category = 0
     cnt_unique_item = 0
-    name: str
-    description: str
-    products: list
 
     def __init__(self, name: str, description: str, products: list):
         self.name = name
         self.description = description
         self.products = products
+        self.cnt_category = []
+        self.cnt_unique_item = len(self.cnt_category)
 
-        Category.cnt_category += 1
-        Category.cnt_unique_item = self.cnt_category
+    def add_cnt_category(self, products):
+        self.cnt_category.append(products)
+
+
+    # def add_category(self, products):
+    #     return products in self.cnt_category
--- tests/__init__.py
+++ tests/__init__.py
@@ -14,21 +14,28 @@ def banana():
 
 
 @pytest.fixture()
-def test_category(apple, banana):
-    return Category('Фрукты', 'ЗОЖ', [apple, banana])
+def test_category():
+    return Category('Фрукты', 'ЗОЖ', [apple, banana], 1, 2)
 
 
 def category_fruit(test_category, apple, banana):
     assert test_category.name == 'Фрукты'
     assert test_category.description == 'ЗОЖ'
     assert test_category.products == [apple, banana]
-
-@pytest.fixture()
-def test_category_unique_item(test_category):
     assert test_category.cnt_category == 1
     assert test_category.cnt_unique_item == 2
 
+# @pytest.fixture()
+# def test_category_unique_item(test_category):
+#     assert test_category.cnt_category == 1
+#     assert test_category.cnt_unique_item == 2
 
+if __name__ == '__main__':
+    cat_1 = Category('Фрукты', 'ЗОЖ', [apple, banana])
+    cat_2 = Category('Овощи', 'ЗОЖ', [apple, banana])
+    cat_3 = Category('Фрукты', 'ЗОЖ', [apple, banana])
+print(f"Общее кол-во категорий: {Category.cnt_category}")
+print(f"Общее кол-во уникальных продуктов: {Category.cnt_unique_item}")
 
 
 
